4514af258cb851586a9f567e5c595fe0823765d3
Michelangelo van Dam <michelangelo@in2it.be>
2017-11-08 08:23:06 +0100

    Now writing the application logic
    Our tests will ensure we write good code

---
+++ src/HelloWorld.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHPUnit Masterclass
+ *
+ * With in2it’s PHPUnit Masterclass you will learn how to test legacy
+ * PHP apps, use test-driven development for new projects and to write
+ * better tests.
+ *
+ * @copyright 2009 - 2017 © In2it. All rights reserved
+ * @license Apache License 2.0 - See LICENSE for details
+ * @see https://www.in2it.be/training-courses/phpunit-masterclass/
+ */
+
+namespace In2it\Masterclass;
+
+
+class HelloWorld
+{
+    /**
+     * This method returns the string "Hello World!"
+     *
+     * @return string
+     */
+    public function sayHello(): string
+    {
+        return 'Hello World!';
+    }
+}
