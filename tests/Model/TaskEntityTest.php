a13fd017a481c58a24eba41a39f4fde8a2a7c049
Michelangelo van Dam <michelangelo@in2it.be>
2017-11-09 21:14:03 +0100

    Creating tests for TaskEntity
    Creating tests for `TaskEntity` and introducing some spectacular features of PHP 7, one I love the most is "strict type checking", making testing so much easier.

---
+++ tests/Model/TaskEntityTest.php
@@ -0,0 +1,161 @@
+<?php
+declare(strict_types=1);
+/**
+ * PHPUnit Masterclass
+ *
+ * With in2it’s PHPUnit Masterclass you will learn how to test legacy
+ * PHP apps, use test-driven development for new projects and to write
+ * better tests.
+ *
+ * @copyright 2009 - 2017 © In2it. All rights reserved
+ * @license Apache License 2.0 - See LICENSE for details
+ * @see https://www.in2it.be/training-courses/phpunit-masterclass/
+ */
+
+namespace In2it\Masterclass\Test\Model;
+
+use In2it\Masterclass\Model\TaskEntity;
+use In2it\Masterclass\Model\TaskEntityInterface;
+use PHPUnit\Framework\TestCase;
+use Faker\Factory;
+
+class TaskEntityTest extends TestCase
+{
+    /**
+     * Test to see that defaults are correctly set when instantiating
+     * a new task entity.
+     *
+     * @covers TaskEntity::__construct
+     * @covers TaskEntity::getId
+     * @covers TaskEntity::getLabel
+     * @covers TaskEntity::getDescription
+     * @covers TaskEntity::isDone
+     * @covers TaskEntity::getCreated
+     * @covers TaskEntity::getModified
+     */
+    public function testTaskEntityIsEmptyAtConstruction()
+    {
+        $task = new TaskEntity();
+        $this->assertInstanceOf(TaskEntityInterface::class, $task);
+        $this->assertSame('', $task->getId());
+        $this->assertSame('', $task->getLabel());
+        $this->assertSame('', $task->getDescription());
+        $this->assertFalse($task->isDone());
+        $this->assertInstanceOf(\DateTime::class, $task->getCreated());
+        $this->assertInstanceOf(\DateTime::class, $task->getModified());
+    }
+
+    /**
+     * Bad data provider returns values to feed to the unit test to check
+     * input validation is done correctly
+     *
+     * @return array
+     */
+    public function wrongDataProvider()
+    {
+        return [
+            [1234, 'This is a test task', 'This test validates the task entity', false],
+            ['2340ee1c-499c-4c31-ac80-0da6f480a2bf', 1234, 'This test validates the task entity', true],
+            ['2340ee1c-499c-4c31-ac80-0da6f480a2bf', 'This is a test task', 1234, false],
+            ['2340ee1c-499c-4c31-ac80-0da6f480a2bf', 'This is a test task', 'This test validates the task entity', 'foo'],
+        ];
+    }
+
+    /**
+     * Test that an error is thrown when wrong type of
+     * arguments are being used to instantiate a new
+     * task entity.
+     *
+     * @covers TaskEntity::__construct
+     * @dataProvider wrongDataProvider
+     * @expectedException \TypeError
+     */
+    public function testTaskEntityThrowsErrorWhenConstructedWithWrongTypeOfArguments(
+        $id,
+        $label,
+        $description,
+        $done
+    )
+    {
+        $task = new TaskEntity($id, $label, $description, $done);
+        $this->fail('Expected exception was not thrown');
+    }
+
+    /**
+     * Bad data provider returns values to feed to the unit test to check
+     * input validation is done correctly
+     *
+     * @return array
+     */
+    public function badDataProvider()
+    {
+        return [
+            ['<script>alert("hacked");</script>', 'This is a test task', 'This test validates the task entity', false],
+        ];
+    }
+
+    /**
+     * Test that an exception is thrown when wrong arguments are
+     * being used to instantiate a new task entity.
+     *
+     * @covers TaskEntity::__construct
+     * @dataProvider badDataProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testTaskEntityThrowsExceptionWhenConstructedWithWrongArguments(
+        $id,
+        $label,
+        $description,
+        $done
+    )
+    {
+        $task = new TaskEntity($id, $label, $description, $done);
+        $this->fail('Expected exception was not thrown');
+    }
+
+    /**
+     * Good data provider returns values to feed to the unit test to check
+     * input validation is done correctly
+     *
+     * @return array
+     */
+    public function goodDataProvider()
+    {
+        $faker = Factory::create();
+        $goodData = [];
+        for ($i = 0; $i < 10; $i++) {
+            $goodData[] = [
+                $faker->uuid,
+                $faker->text(150),
+                $faker->sentence(),
+                $faker->boolean,
+            ];
+        }
+        return $goodData;
+    }
+
+    /**
+     * Test that valid arguments are correctly processed in the entity
+     *
+     * @covers TaskEntity::__construct
+     * @covers TaskEntity::getId
+     * @covers TaskEntity::getLabel
+     * @covers TaskEntity::getDescription
+     * @covers TaskEntity::isDone
+     * @dataProvider goodDataProvider
+     */
+    public function testTaskEntityAcceptsCorrectArguments(
+        $id,
+        $label,
+        $description,
+        $done
+    )
+    {
+        $task = new TaskEntity($id, $label, $description, $done);
+        $this->assertInstanceOf(TaskEntityInterface::class, $task);
+        $this->assertSame($id, $task->getId());
+        $this->assertSame($label, $task->getLabel());
+        $this->assertSame($description, $task->getDescription());
+        $this->assertSame($done, $task->isDone());
+    }
+}
